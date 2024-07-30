feb5db45c3450cda4774d895daaf4e601354e74b
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-07-30 21:05:44 +0500

    solve button issues

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const floors = [
 let container = document.querySelector(".building-container");
 let elevator = document.querySelector(".elevator");
 let currentFloor = 1;
+let processing = false;
+let arr = [];
 
 floors.forEach((e, i) => {
   container.innerHTML += `<div class='floor'><button class="elevator-btn">${e.number}</button></div>`;
@@ -28,19 +30,29 @@ const elevatorBtns = document.querySelectorAll(".elevator-btn");
 
 elevatorBtns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
-    let comingFloor = btn.innerText;
-    elevator.style.transition = `${Math.abs(
-      comingFloor - currentFloor
-    )}s linear`;
-    elevator.style.bottom = floors[i].position;
-
-    btn.disabled = true;
-    btn.style.backgroundColor = "#074c7767";
-
-    setTimeout(() => {
-      currentFloor = btn.innerText;
-      btn.disabled = false;
-      btn.style.backgroundColor = "#074c77";
-    }, Math.abs(comingFloor - currentFloor) * 1000);
+    elevatorMoving(btn, i);
+    arr.push(parseInt(btn.innerText));
+    console.log(arr);
   });
 });
+const elevatorMoving = (btn, i) => {
+  if (processing) return;
+  processing = true;
+  arr.shift();
+  let comingFloor = btn.innerText;
+  elevator.style.transition = `${Math.abs(comingFloor - currentFloor)}s linear`;
+  elevator.style.bottom = floors[i].position;
+  btn.disabled = true;
+  btn.style.backgroundColor = "#074c7767";
+  setTimeout(() => {
+    currentFloor = btn.innerText;
+    setTimeout(() => {
+      processing = false;
+      btn.disabled = false;
+      btn.style.backgroundColor = "#074c77";
+      if (arr.length > 0) {
+        elevatorMoving(btn, i);
+      }
+    }, 1000);
+  }, Math.abs(comingFloor - currentFloor) * 1000);
+};
