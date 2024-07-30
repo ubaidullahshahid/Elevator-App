f64db86469bde52ec886d7703d0131cca0c2f2fa
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-07-31 02:30:45 +0500

    complete all main features

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const floors = [
 ];
 let container = document.querySelector(".building-container");
 let elevator = document.querySelector(".elevator");
+let leftDoor = document.querySelector(".left-door");
+let rightDoor = document.querySelector(".right-door");
+let door = document.querySelectorAll(".door");
 let currentFloor = 1;
 let processing = false;
 let arr = [];
@@ -30,29 +33,32 @@ const elevatorBtns = document.querySelectorAll(".elevator-btn");
 
 elevatorBtns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
-    elevatorMoving(btn, i);
-    arr.push(parseInt(btn.innerText));
-    console.log(arr);
+    btn.style.backgroundColor = "darkgreen";
+    arr.push({ number: parseInt(btn.innerText), index: i });
+    elevatorMoving();
   });
 });
-const elevatorMoving = (btn, i) => {
-  if (processing) return;
+const elevatorMoving = () => {
+  if (processing || arr.length === 0) return;
+  const { number: comingFloor, index } = arr.shift();
   processing = true;
-  arr.shift();
-  let comingFloor = btn.innerText;
+  elevatorBtns[index].style.backgroundColor = "#074c7767";
+  elevatorBtns[index].disabled = true;
   elevator.style.transition = `${Math.abs(comingFloor - currentFloor)}s linear`;
-  elevator.style.bottom = floors[i].position;
-  btn.disabled = true;
-  btn.style.backgroundColor = "#074c7767";
+  elevator.style.bottom = floors[index].position;
   setTimeout(() => {
-    currentFloor = btn.innerText;
+    currentFloor = elevatorBtns[index].innerText;
+    leftDoor.style.left = "-50%";
+    rightDoor.style.right = "-50%";
     setTimeout(() => {
+      leftDoor.style.left = "0px";
+      rightDoor.style.right = "0px";
+    }, 2000);
+    setTimeout(() => {
+      elevatorBtns[index].disabled = false;
+      elevatorBtns[index].style.backgroundColor = "#074c77";
       processing = false;
-      btn.disabled = false;
-      btn.style.backgroundColor = "#074c77";
-      if (arr.length > 0) {
-        elevatorMoving(btn, i);
-      }
-    }, 1000);
+      elevatorMoving();
+    }, 4000);
   }, Math.abs(comingFloor - currentFloor) * 1000);
 };
